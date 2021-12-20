5f645004b594ca8e4b753ea8864d3e2db4253bcd
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-20 18:28:44 +0530

    Assignment 6

---
+++ Prime_Number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Prime_Number{
+	public static void main(String[] args){
+
+		int n,count = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		n = sc.nextInt();
+		for(int i=1; i<=n; i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+			}
+		}
+		if(count==2){
+			System.out.println("Prime Number");
+		}
+			else
+			{
+				System.out.println("Not Prime Number");
+			}
+		}
+		
+	}
+
+		
